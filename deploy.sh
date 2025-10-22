500934375309208190ee796c5ae049c3312d48d7
Kome Udogu <kay1.kom@gmail.com>
2025-10-22 14:07:34 +0100

    Enhance deploy script with user input prompts
    Added user input prompts for Git repo URL, personal access token, SSH username, IP address, SSH key path, and application port. Updated SSH commands to install and enable Docker and Nginx.

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,24 @@ set -e
 set -u
 set -o pipefail
 #
-# 
+# User input 
 read -p "Git Repo URL:" REPO_URL
+read -p "Personal Access Token: " PAT
+read -p "SSH Username" SSH_UNAME
+read -p "SSH IP Address" SSH_IP
+read -p "SSH Key path" SSH_KEY
+read -p "Internal Container Application Port" APP_PORT
+#
+# SSH into host and update/ install required appps
+#
+ssh -i "$SSH_KEY" "$SSH_UNAME@$SSH_IP" 
+<<EOF
+sudo apt update -y
+sudo apt install -y docker.io nginx
+sudo systemctl enable --now docker nginx
+rm -rf ~/app && mkdir ~/appsudo apt update -y
+sudo apt install -y docker.io nginx
+sudo systemctl enable --now docker nginx
+rm -rf ~/app && mkdir ~/app
+
+EOF
